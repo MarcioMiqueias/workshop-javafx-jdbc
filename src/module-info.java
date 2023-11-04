2f1861f5f89fcf0d7631feade054b58b6f8c7ef3
Proxx <mmgi.marcio@gmail.com>
2023-11-04 16:28:06 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,15 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	
+
 	exports gui;
+
 	opens gui to javafx.fxml;
-	
+
 	opens application to javafx.graphics, javafx.fxml;
+
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+	
+	opens model.services to javafx.graphics, javafx.fxml;
+
 }
