891bf3d6eb49709eb7fb097383fc335f526a8829
Proxx <mmgi.marcio@gmail.com>
2023-12-03 12:47:10 -0300

    Saving a new Department

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,12 @@
 module workshop_javafx_jdbc {
+	requires transitive javafx.base;
+
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires java.sql;
 
+	
 	exports gui;
 
 	opens gui to javafx.fxml;
